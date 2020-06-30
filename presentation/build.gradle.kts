9b6b3afebcba32c133bea7b9fa69df68a6fa77ba
Alexander <alexander.but-gusaim@softeq.com>
2020-06-30 14:24:46 +0300

    initial commit

---
+++ presentation/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id(BuildPlugins.androidApplication)
+    id(BuildPlugins.kotlinAndroid)
+    id(BuildPlugins.kotlinAndroidExtensions)
+}
+
+android {
+    compileSdkVersion(AndroidSdk.compile)
+    defaultConfig {
+        applicationId = null
+        minSdkVersion(AndroidSdk.min)
+        targetSdkVersion(AndroidSdk.target)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+
+dependencies {
+
+    implementation(Libraries.kotlinStdLib)
+    implementation(Libraries.appCompat)
+    implementation(Libraries.ktxCore)
+    implementation(Libraries.constraintLayout)
+    implementation("androidx.appcompat:appcompat:1.1.0")
+    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+
+    testImplementation (TestLibraries.junit4)
+    androidTestImplementation (TestLibraries.testRunner)
+    androidTestImplementation (TestLibraries.espresso)
+}
