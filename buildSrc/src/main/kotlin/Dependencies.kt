7b7498cbb6380093cf243a6900b95670ad07483c
barkatme <alexander.butgusaim@gmail.com>
2020-07-02 18:24:54 +0300

    kotlin plugin refactor

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -9,6 +9,7 @@ object BuildPlugins {
     const val androidGradlePlugin = "com.android.tools.build:gradle:${Versions.buildToolsVersion}"
     const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion"
     const val androidApplication = "com.android.application"
+    const val kotlin = "kotlin"
     const val kotlinAndroid = "kotlin-android"
     const val kotlinAndroidExtensions = "kotlin-android-extensions"
 
--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins{
-    id("kotlin")
+    id(BuildPlugins.kotlin)
 }
 
 dependencies {
