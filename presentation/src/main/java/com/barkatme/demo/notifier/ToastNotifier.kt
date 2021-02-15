ab8fc7ece13109c7408c3abbf60d33f393c96c8a
barkatme <alexander.butgusaim@gmail.com>
2021-02-15 11:28:07 +0300

    chat button text "send"

--- presentation/src/main/java/com/barkatme/demo/notifier/ToastNotifier.kt
+++ presentation/src/main/java/com/barkatme/demo/notifier/ToastNotifier.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import android.widget.Toast
 import com.barkatme.demo.domain.usecase.demo.notifier.Notifier
 
-class ToastNotifier(private val notifier: Notifier, private val context: Context) : Notifier {
+class ToastNotifier(
+    private val notifier: Notifier,
+    private val context: Context
+) : Notifier {
     override fun notify(message: String) {
         notifier.notify(message)
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
